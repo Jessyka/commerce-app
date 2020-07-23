8f71db4b6e5d65cf5fc2aca544caf2c358d2cf16
Jessyka Lage <jessykalage@gmail.com>
2020-07-23 13:21:22 -0300

    install storebook knobs

--- src/components/Main/main.stories.tsx
+++ src/components/Main/main.stories.tsx
@@ -1,8 +1,18 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from './index'
 
 export default {
   title: 'Main page',
+  decorators: [withKnobs],
   component: Main
 }
 
-export const Basic = () => <Main />
+export const Basic = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Components'
+    )}
+  />
+)
